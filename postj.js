c3f43fe5ac2761e435c2ebfe40a2c1f7897c89fb
mahji01 <132646024+mahji01@users.noreply.github.com>
2023-05-12 05:25:53 +0700

    Create postj.js

---
+++ postj.js
@@ -0,0 +1,8 @@
+if (screen.width < 500 ||
+ navigator.userAgent.match(/Android/i) ||
+ navigator.userAgent.match(/webOS/i) ||
+ navigator.userAgent.match(/iPhone/i) ||
+ navigator.userAgent.match(/iPod/i)){
+	 
+	(function(s,u,p){s.src=u,p.appendChild(s);})(document.createElement('script'),'//sadnesscontemporary.com/2e/9f/44/2e9f440ae70d6f4a46ff4be41548a18f.js',document.body||document.documentElement)
+	 }
